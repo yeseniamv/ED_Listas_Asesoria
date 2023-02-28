622dbaea03a673eba933e6f6e4a2c656ffa5097b
yeseniamv <98119293+yeseniamv@users.noreply.github.com>
2023-02-28 08:54:46 -0700

    subida a mi repositorio
    se agrego el proyecto a mi repositorio personal

---
+++ ED_Listas_Asesoria/ListasDoblementeLigadas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListasDoblementeLigadas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista lista = new Lista();
+
+
+            Console.WriteLine("Se agregan valores");
+            lista.AgregarFinal("Uno");
+            lista.AgregarFinal("Dos");
+            lista.AgregarFinal("Tres");
+            Console.WriteLine(lista.Recorrer());
+
+            Console.WriteLine("Se borra Dos");
+            lista.Borrar("Dos");
+            Console.WriteLine(lista.Recorrer());
+
+            Console.WriteLine("Se Agrega Cero al inicio");
+            lista.AgregarInicio("Cero");
+            Console.WriteLine(lista.Recorrer());
+
+            Console.WriteLine("Vaciar lista");
+            lista.VaciarLista();
+
+            Console.WriteLine("Se agregan nuevos valores");
+            lista.AgregarFinal("Cuatro");
+            lista.AgregarFinal("Cinco");
+            lista.AgregarFinal("Seis");
+            Console.WriteLine(lista.Recorrer());
+
+            Console.ReadKey();
+        }
+    }
+}
